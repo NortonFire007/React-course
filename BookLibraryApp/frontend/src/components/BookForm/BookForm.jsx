030e8b76c6ed13c59ee59d917b37af6e6789829b
NortonFire007 <rioknuto@gmail.com>
2024-04-25 23:20:43 +0300

    Initialize Redux, add dispalay books

--- BookLibraryApp/frontend/src/components/BookForm/BookForm.jsx
+++ BookLibraryApp/frontend/src/components/BookForm/BookForm.jsx
@@ -1,18 +1,27 @@
 import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/actionCreators';
 import './BookForm.css';
 
 const BookForm = () => {
   const [title, setTilte] = useState('');
   const [author, setAuthor] = useState('');
+  const dispatch = useDispatch();
 
   const handleSubmit = (e) => {
     e.preventDefault();
 
     if (title && author) {
+      const book = {
+        title,
+        author,
+      };
+
+      dispatch(addBook(book));
+
       setTilte('');
       setAuthor('');
     }
-    // dispatch
   };
 
   return (
--- BookLibraryApp/frontend/src/components/BookList/BookList.jsx
+++ BookLibraryApp/frontend/src/components/BookList/BookList.jsx
@@ -1,9 +1,25 @@
+import { useSelector } from 'react-redux';
 import './BookList.css';
 
 const BookList = () => {
+  const books = useSelector((state) => state.books);
+
   return (
     <div className="app-block book-list">
       <h2>Book List</h2>
+      {!books.length ? (
+        <p>No books availale</p>
+      ) : (
+        <ul>
+          {books.map((book, ind) => (
+            <li key={ind}>
+              <div className="book-info">
+                {++ind}. {book.title} by <strong>{book.author}</strong>
+              </div>
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
